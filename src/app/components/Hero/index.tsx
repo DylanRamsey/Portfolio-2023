9e2848315532a5eb474915938089421b9523caea
Dylan <dylanbradramsey@gmail.com>
2023-11-27 13:02:35 -0500

    More work on hero section, added type writter effect, some other hover animations

--- src/app/components/Hero/index.tsx
+++ src/app/components/Hero/index.tsx
@@ -1,4 +1,6 @@
+'use client'
 import Image from 'next/image'
+import Typewriter from "typewriter-effect";
 
 export default function Hero() {
   return (
@@ -8,15 +10,24 @@ export default function Hero() {
           Dylan Ramsey
         </h1>
         <h2 className="text-6xl mb-8">
-          Web Developer
+          Full-Stack Web Engineer
         </h2>
+        <h3 className="text-5xl mb-8">
+          <Typewriter
+            options={{
+              strings: ['Developer. . .', 'Designer. . .', 'UX Strategist. . .', 'Consultant. . .', 'Digital Marketer. . .', 'Technologist. . .', 'Gamer. . .'],
+              autoStart: true,
+              loop: true,
+            }}
+          />            
+        </h3>
         <h3 className="text-3xl mb-8">
           Sarasota, Florida
         </h3>
         <h3 className="text-3xl mb-8">
           Get in Touch 
           <Image
-            className="inline mx-3"
+            className="inline mx-3 transition ease-in-out delay-150 hover:-translate-y-1 duration-300"
             width={40}
             height={40}
             src={"/icons/envelope.svg"}
@@ -26,7 +37,7 @@ export default function Hero() {
         <h3 className="text-3xl mb-8">
           Check me Out!
           <Image
-            className="inline mx-3"
+            className="inline mx-3 transition ease-in-out delay-150 hover:-translate-y-1 duration-300"
             width={35}
             height={35}
             src={"/icons/down-arrow.svg"}
@@ -35,7 +46,7 @@ export default function Hero() {
         </h3>        
       </div>
       <Image
-        className="block"
+        className="block object-contain"
         width={435}
         height={458}
         src={"/images/dylan_and_data_website_profile.webp"}
--- src/app/components/Navigation/index.tsx
+++ src/app/components/Navigation/index.tsx
@@ -5,17 +5,17 @@ export default function Navigation() {
   return (
     <nav className="container mx-auto mb-40 flex flex-row justify-between items-baseline">
       <ul className="flex flex-row gap-x-8 mt-16">
-        <li>
+        <li className="transition ease-in-out delay-150 hover:-translate-y-1 duration-300">
           <Link href="/">Home</Link>
         </li>
-        <li>
+        <li className="transition ease-in-out delay-150 hover:-translate-y-1 duration-300">
           <Link href="/portfolio">Portfolio</Link>
         </li>      
-        <li>About</li>
-        <li>Blog</li>
+        <li className="transition ease-in-out delay-150 hover:-translate-y-1 duration-300">About</li>
+        <li className="transition ease-in-out delay-150 hover:-translate-y-1 duration-300">Blog</li>
       </ul>
       <ul className="flex gap-4">
-        <a href="https://github.com/DylanRamsey" target="_blank">
+        <a href="https://github.com/DylanRamsey" target="_blank" className="transition ease-in-out delay-150 hover:-translate-y-1 duration-300">
           <Image
             width={35}
             height={35}
@@ -23,7 +23,7 @@ export default function Navigation() {
             alt="Github Icon"
           />  
         </a>
-        <a href="https://www.linkedin.com/in/dylan-ramsey/" target="_blank">
+        <a href="https://www.linkedin.com/in/dylan-ramsey/" target="_blank" className="transition ease-in-out delay-150 hover:-translate-y-1 duration-300">
           <Image
             width={35}
             height={35}
