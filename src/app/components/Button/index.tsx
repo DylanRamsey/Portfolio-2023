69be2c3bdaef323adf2f4fce7aca1fe742fee6e3
Dylan <dylanbradramsey@gmail.com>
2024-06-11 15:28:49 -0400

    Doing the Typescript thing

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -1,7 +1,12 @@
-export default function Button({buttonText}) {
+interface Props {
+  buttonText: string,
+}
+
+
+export default function Button(props: Props) {
   return (
     <div className="mx-auto text-4xl border-white border-4 max-w-max px-4 py-2">
-      {buttonText}
+      {props.buttonText}
     </div>
   )
 }
--- src/app/components/ProjectCard/index.tsx
+++ src/app/components/ProjectCard/index.tsx
@@ -2,26 +2,33 @@ import IconOpenLink from '../icons/IconOpenLink'
 import GitHubIcon from '../icons/GitHubIcon'
 import FigamIcon from '../icons/FigmaIcon'
 
-export default function ProjectCard({projectName, projectLink, projectScreenshot}) {
+interface Props {
+  projectName: string,
+  projectLink: string,
+  projectScreenshot: string,
+}
+
+
+export default function ProjectCard(props: Props) {
   return (
     <section>
       <div className='block mx-auto lg:w-72	xl:w-96'>
         <div className="mx-auto text-center mb-8 rounded-lg bg-white shadow-xl">
           <div className="aspect-video">
-            {projectScreenshot ?
+            {props.projectScreenshot ?
               <img
                 className='min-h-full object-cover rounded-t-lg'
-                src={projectScreenshot}
-                alt={projectName}
+                src={props.projectScreenshot}
+                alt={props.projectName}
               />
               : null
             }
           </div>
           <div className="project_card_content flex flex-col justify-center text-2xl border-solid border-t-2 border-black h-24">
-            <p className="project_card_content__title text-black text-2xl mb-2">{projectName}</p>
+            <p className="project_card_content__title text-black text-2xl mb-2">{props.projectName}</p>
             <div className="project_card_content__links flex justify-center items-center rounded-lg gap-2">
-            {projectLink ?
-              <a href={projectLink} target="_blank">
+            {props.projectLink ?
+              <a href={props.projectLink} target="_blank">
                 <IconOpenLink iconHeight={30} color="black" />
               </a>
               : null
