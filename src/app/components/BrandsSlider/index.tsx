7d24acf3351cc913808b84d7e3feb49d145b1de7
Dylan <dylanbradramsey@gmail.com>
2024-06-11 19:40:27 -0400

    trying to find where weird error is coming from

--- src/app/components/BrandsSlider/index.tsx
+++ src/app/components/BrandsSlider/index.tsx
@@ -1,11 +1,9 @@
 "use client";
-import ProjectCard from '@/app/components/ProjectCard'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/react';
 import 'swiper/css';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import { Navigation } from 'swiper/modules';
 import brandImages from "@/app/data/brandImagePaths.js"
 import Image from 'next/image'
 
@@ -13,20 +11,15 @@ import Image from 'next/image'
 export default function BrandsSlider() {
   return (
     <section className="container mx-auto mb-44">
-      <h2 className="text-3xl lg:text-6xl text-center mb-20">Brands I've worked with</h2>
+      <h2 className="text-3xl lg:text-6xl text-center mb-20">Brands Ive worked with</h2>
       <Swiper
         navigation={true}
-        modules={[Navigation]}
+
         slidesPerView={1}
         spaceBetween={30}
-        breakpoints={{
-          768: {
-            slidesPerView: 3,
-          },
-        }}
         className="mySwiper mb-8">
         {brandImages.map((brand, index) => (
-          <SwiperSlide key={index} className='my-auto'>
+          <SwiperSlide key={index} className="my-auto">
             <Image className="object-contain ml-16"
               src={brand.link}
               alt={brand.alt}
