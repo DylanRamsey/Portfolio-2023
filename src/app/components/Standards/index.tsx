efc5cc0da11a88e09c898d667d8ed54c12ce18d5
Dylan <dylanbradramsey@gmail.com>
2024-06-11 20:02:12 -0400

    More cleanup, build working locally

--- src/app/components/Standards/index.tsx
+++ src/app/components/Standards/index.tsx
@@ -1,36 +1,18 @@
 "use client";
 import Image from 'next/image';
-import ScreenResponsive from "@/app/components/icons/ScreenResponsive";
-import AccessibleIcon from "@/app/components/icons/AccessibleIcon";
-import SpeedGuage from "@/app/components/icons/SpeedGuage";
-
+/* Work in progress file */
 export default function BrandsSlider() {
   return (
     <section className="container mx-auto mb-24">
       <h2 className="text-3xl lg:text-6xl text-center mb-20">All of my work is...</h2>
       <div className="flex align-center justify-evenly">
         <div className="">
-          <ScreenResponsive
-            iconHeight={200}
-            iconWidth={196}
-            color="white"
-          />
           <h4>Fully mobile responsive</h4>
         </div>
         <div className="">
-          <AccessibleIcon
-            iconHeight={180}
-            iconWidth={196}
-            color="white"
-          />
           <h4>Accessible to all users</h4>
         </div>
         <div className="">
-          <SpeedGuage
-            iconHeight={200}
-            iconWidth={196}
-            color="white"
-          />
           <h4>Optimized to be lightning fast</h4>
         </div>
       </div>
